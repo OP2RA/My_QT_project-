ba0f83f51526011798f547599b9b309213813203
OP2RA <75778101+OP2RA@users.noreply.github.com>
2021-11-20 02:38:22 +0300

    Update main.py

--- My_PyQt5_Project/main.py
+++ My_PyQt5_Project/main.py
@@ -2,11 +2,8 @@ import sys
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog
-#from PyGame import Pazzle
 
-import pygame_front
 
-global folder
 class MainScreen(QDialog):
     def __init__(self):
         super().__init__()
@@ -16,8 +13,8 @@ class MainScreen(QDialog):
         self.pushButton_2.clicked.connect(self.getFileNames)
 
     def getFileNames(self):
-        filenames = QFileDialog.getOpenFileNames(self, '', '.')
-        self.folder = filenames
+        self.file = Creater()
+        self.file.showphoto()
 
 
     def goinside(self):
@@ -26,34 +23,57 @@ class MainScreen(QDialog):
         self.hide()
 
 
-
 class Creater(MainScreen):
     def __init__(self):
         super(Creater, self).__init__()
-
         loadUi('Creater_1.ui', self)
         self.pushButton.clicked.connect(self.returnWid)
         self.pushButton_2.clicked.connect(self.showphoto)
         self.pushButton_4.clicked.connect(self.start)
 
     def showphoto(self):
-        filenames = QFileDialog.getOpenFileNames(self, '', '.')
-        self.folder = filenames
-        image = QPixmap(self.folder[0][0])
-        self.label_5.setPixmap(image)
+        try:
+            filenames = QFileDialog.getOpenFileNames(self, '', '.')
+            self.folder = filenames[0][0]
+            if str(self.folder[len(self.folder) - 3:]) != 'png':
+                raise ImportError
+            else:
+                self.image = QPixmap(self.folder)
+                self.label.setPixmap(self.image)
+                # НЕ НУЖНО:
+                # try_find_name = []
+                # name = ''
+                # for i in range(1, len(str(self.folder))):
+                #     if self.folder[-i] != '/':
+                #         try_find_name.append(self.folder[-i])
+                #     else:
+                #         break
+                # for i in range(1, len(try_find_name) + 1):
+                #     name += try_find_name[-i]
+                # self.image = name
+                # self.ready()
+        except ImportError:
+            self.err = Error()
+            self.err.show()
+
+    # def ready(self):
+    #     res = self.image
 
     def start(self):
-        pygame_front.start_game()
-
+        import pygame_front
 
     def returnWid(self):
         self.returnView = MainScreen()
         self.returnView.show()
         self.hide()
 
-
-
-
+class Error(QDialog):
+    def __init__(self):
+        super().__init__()
+        loadUi('error_notific.ui', self)
+        self.pushButton.clicked.connect(self.hidewid)
+    def hidewid(self):
+        self.hide()
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
@@ -63,6 +83,7 @@ if __name__ == '__main__':
     form.show()
     sys.excepthook = except_hook
     sys.exit(app.exec())
+
 
 
 
